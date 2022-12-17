2df1e060500d9491f1153bdd069f8bed05e1d71f
colindickson <colin.dickson@gmail.com>
2022-12-17 14:56:31 -0500

    wip mints

--- Makefile
+++ Makefile
@@ -3,5 +3,13 @@ protogen:
 	substreams protogen ./substreams.yaml
 
 .PHONY: build
-build:
+build: protogen
 	cargo build --target wasm32-unknown-unknown --release
+
+.PHONY: pack
+pack: build
+	substreams pack ./substreams.yaml
+
+.PHONY: run
+run: pack
+	substreams run ./trumpy-v3-v0.0.0.spkg map_mint_several -e polygon.streamingfast.io:443 -s 36860752 -t 36860852
--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,61 @@ mod pb;
 
 use substreams::errors::Error;
 use substreams::{log, Hex};
+use substreams_ethereum::Function;
 use substreams_ethereum::pb::eth::v2::Block;
-use crate::pb::trumpy::{Transfer, Transfers};
+use crate::pb::trumpy::{Mints, Transfer, Transfers};
 use crate::utils::FACTORY_ADDRESS;
 
 
 
 #[substreams::handlers::map]
 pub fn map_transfer_ownership(block: Block) -> Result<pb::trumpy::Transfers, Error> {
-    use abi::factory::events::OwnershipTransferred;
+    use abi::factory::events::Transfer;
 
     Ok(pb::trumpy::Transfers {
-        transfers: block.events::<OwnershipTransferred>(&[&FACTORY_ADDRESS])
+        transfers: block.events::<Transfer>(&[&FACTORY_ADDRESS])
             .filter_map(|(event, log)| {
                 Some(pb::trumpy::Transfer {
-                    previous_owner: Hex(event.previous_owner).to_string(),
-                    new_owner: Hex(event.new_owner).to_string(),
+                    from: Hex(event.from).to_string(),
+                    to: Hex(event.to).to_string(),
+                    token_id:event.token_id.to_string(),
                 })
             }).collect()
+    })
+}
+
+#[substreams::handlers::map]
+pub fn map_mint_several(block: Block) -> Result<pb::trumpy::Mints, Error> {
+    use abi::factory::functions::MintSeveral;
+
+    let mut mints = vec![];
+
+
+    for trx in block.transaction_traces {
+        if trx.status != 1 {
+            continue;
+        }
+
+        for call in trx.calls {
+            if call.address != FACTORY_ADDRESS {
+                continue;
+            }
+            if MintSeveral::match_call(&call) {
+                match MintSeveral::decode(&call) {
+                    Ok(mint) => {
+                        mints.push(pb::trumpy::Mint {
+                            minter: Hex(mint.minter).to_string(),
+                            number_of_tokens: mint.number_of_tokens.to_u64() as i64,
+                        });
+                    }
+                    Err(_) => {}
+                }
+            }
+        }
+    }
+
+
+    Ok(Mints {
+        mints,
     })
 }
